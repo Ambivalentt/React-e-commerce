195781aaf2a87060c39fb34dcefc17fed997d7d9
Martin H <martinhj.sama@gmail.com>
2023-08-20 15:39:21 -0500

    PreEntrega1 Hilarion

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Navbar from './components/NavBar'
+import './index.css'
+function App() {
+  return (
+    <Navbar/>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import './Styles/CustomStyles.css'
+import './Styles/NavBar.css'
+import { BsCart3 } from "react-icons/bs";
+import { BiMenu } from "react-icons/bi";
+import { useState, useEffect } from 'react'
+
+
+const Navbar = () => {
+    const [theme, setTheme] = useState(false);
+    const [menuBtn, setMenuBtn] = useState(false);
+
+    const handleMenuBtn = () =>{
+        setMenuBtn(prevStatus => !prevStatus)
+    }
+
+    const handleTheme = () => {
+        setTheme((prevTheme) => !prevTheme);
+    }
+
+    return (
+        <nav className={`flex items-center justify-between transition-all duration-500 px-2 h-16 ${theme ? 'bg-black' : 'bg-gray-200'}`}>
+            <div className={`block md:hidden relative text-5xl ${theme ? 'text-white' : 'text-black'}`}>
+                <BiMenu onClick={handleMenuBtn} />
+                <ul className={`font_rubik -left-2 ps-4 overflow-hidden absolute rounded-b-lg transitio w-40 transition-all duration-500 flex-col select-none text-xl 
+                ${theme ? 'default_theme_Navbar' && 'bg-black' : 'text-black' && 'bg-gray-200'} 
+                ${menuBtn ? 'navbarMobileOpen': 'navbarMobileClose'}`}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Promotions</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <section className="flex items-center h-full">
+                <a href="#" className={`font-bold text-2xl pb-1.5 ${theme ? 'text-white' : 'text-black'}`}>Logo</a>
+            </section>
+            <ul className={` hidden font_rubik font-semibold md:flex items-center gap-x-5 select-none ${theme ? 'default_theme_Navbar' : 'text-black'}`}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Promotions</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <section className='flex items-center gap-x-4'>
+                <div className='relative'>
+                    <BsCart3 className={`text-4xl relative ${theme ? 'text-white' : 'text-black'}`} />
+                    <span className='absolute -top-2 -right-2 bg-red-500 rounded-full w-5 text-center text-white font-bold select-none'>0</span>
+                </div>
+
+                <div className={`darkTheme_Container border ${theme ? 'border-white' : 'border-black'}`} onClick={handleTheme}>
+                    <div className={`darkTheme_toggle ${theme ? 'active_darktheme' : 'inactive_darktheme'}`}></div>
+                </div>
+            </section>
+        </nav>
+    )
+}
+
+export default Navbar
