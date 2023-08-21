021aa22c63999f5913f6cc37e166d739126c9cce
Martin H <martinhj.sama@gmail.com>
2023-08-21 16:40:00 -0500

    PreEntrega1 Hilarion

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -21,25 +21,25 @@ const Navbar = () => {
         <nav className={`flex items-center justify-between transition-all duration-500 px-2 h-16 ${theme ? 'bg-black' : 'bg-gray-200'}`}>
             <div className={`block md:hidden relative text-5xl ${theme ? 'text-white' : 'text-black'}`}>
                 <BiMenu onClick={handleMenuBtn} />
-                <ul className={`font_rubik -left-2 ps-4 overflow-hidden absolute rounded-b-lg transitio w-40 transition-all duration-500 flex-col select-none text-xl 
+                <ul className={`font_rubik -left-2 pt-4 ps-4 overflow-hidden absolute rounded-b-lg transitio w-40 transition-all duration-500 flex-col select-none text-xl 
                 ${theme ? 'default_theme_Navbar' && 'bg-black' : 'text-black' && 'bg-gray-200'} 
                 ${menuBtn ? 'navbarMobileOpen': 'navbarMobileClose'}`}>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Products</a></li>
-                    <li><a href="#">Promotions</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a className='underline-animation' href="#">Home</a></li>
+                    <li><a className='underline-animation' href="#">Products</a></li>
+                    <li><a className='underline-animation' href="#">Promotions</a></li>
+                    <li><a className='underline-animation' href="#">About Us</a></li>
+                    <li><a className='underline-animation' href="#">Contact</a></li>
                 </ul>
             </div>
             <section className="flex items-center h-full">
                 <a href="#" className={`font-bold text-2xl pb-1.5 ${theme ? 'text-white' : 'text-black'}`}>Logo</a>
             </section>
-            <ul className={` hidden font_rubik font-semibold md:flex items-center gap-x-5 select-none ${theme ? 'default_theme_Navbar' : 'text-black'}`}>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Products</a></li>
-                <li><a href="#">Promotions</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact</a></li>
+            <ul className={`hidden font_rubik font-semibold md:flex items-center gap-x-5 select-none ${theme ? 'default_theme_Navbar' : 'text-black'}`}>
+                <li className='underline-animation'><a href="#">Home</a></li>
+                <li className='underline-animation'><a href="#">Products</a></li>
+                <li className='underline-animation'><a href="#">Promotions</a></li>
+                <li className='underline-animation'><a href="#">About Us</a></li>
+                <li className='underline-animation'><a href="#">Contact</a></li>
             </ul>
             <section className='flex items-center gap-x-4'>
                 <div className='relative'>
